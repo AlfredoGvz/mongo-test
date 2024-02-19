c3b4a1fb9dfbf240e68bbe40d8c3e0650dbd4b5d
SamsonThompson <samsonthompson@samsontpsonsair.home>
2024-02-19 17:01:28 +0000

    add first two tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,36 +1,39 @@
 const seedTestData = require('../seeds/seed-test')
-const app = require('../app/app')
+const { app } = require('../app/app')
 const request = require('supertest')
+// const database = require('../app/connection')
 
 
-beforeAll(() => seedTestData());
+beforeEach(() => seedTestData());
 
-
-describe('/api/cities', () => {
+describe('/api/cities', (req, res) => {
     it('should respond with an array of city objects witht the correct properties', () => {
         return request(app)
         .get('/api/cities')
         .expect(200)
-        .then(() => {
-            expect(Array.isArray(response.body)).toBe(true);
-            expect(response.body.length).toBeGreaterThan(0);
-            expect(response.body[0]).toHaveProperty('name', 'Manchester');
-            expect(response.body[0]).toHaveProperty('latitude', 53.4794892);
-            expect(response.body[0]).toHaveProperty('longitude', -2.2451148);
+        .then((res) => {
+            console.log(res._body, '<<< the response in the test');
+            const {cities} = res._body
+            // console.log(cities, '<<<cities');
+            expect(cities.length).toBe(10)
+            expect(cities[0]).toEqual(
+            expect.objectContaining({
+                _id: expect.any(String),
+                latitude: 53.4794892,
+                longitude: -2.2451148,
+                name: 'Manchester',
+                display_name: 'Manchester, Greater Manchester, England, United Kingdom',
+                __v: 0
+                })
+            )})
+        })
+        it.only('should respond with a 404 status code when given an incorrectly spelt endppint', () => {
+            return request(app)
+            .get('/api/citiez')
+            .expect(404)
+            .then((body) => {
+                console.log(body.statusCode, '<<<<body status code in test');
+                expect(body.statusCode).toBe(404)
+            })      
         })
     })
-})
-
-
-        // .then(({body}) => {
-        //     const topics = body.topics
-        //     expect(topics.length).toBeGreaterThan(0)
-        //     topics.forEach((element) => {
-        //         expect(element).toEqual(
-        //             expect.objectContaining({
-        //                 slug: expect.any(String),
-        //                 description: expect.any(String)
-        //             })
-//         })
-//     })
-// })
